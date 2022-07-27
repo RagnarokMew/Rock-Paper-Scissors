33e6f3d3a27f051755953b6886278e36f47c1d71
RagnarokMew <temmieisalmightygod@gmail.com>
2022-07-27 12:18:11 +0300

    Add RPS game (pure js)

--- rps.js
+++ rps.js
@@ -1,3 +1,7 @@
+const WIN = true;
+const LOST = false;
+const DRAW = null;
+
 function computerChoice(max)
 {
     return Math.floor(Math.random()*max);
@@ -5,26 +9,61 @@ function computerChoice(max)
 
 function round(playerChoice, computerChoice)
 {
-    const ROUNDWON = true;
-    const ROUNDLOST = false;
-    const DRAW = null;
-
     if(playerChoice == 1)
     {
         if(computerChoice == 1) return DRAW;
-        else if(computerChoice == 2) return ROUNDLOST;
-        return ROUNDWON;
+        else if(computerChoice == 2) return LOST;
+        return WIN;
     }
     else if(playerChoice == 2)
     {
-        if(computerChoice == 1) return ROUNDWON;
+        if(computerChoice == 1) return WIN;
         else if(computerChoice == 2) return DRAW;
-        return ROUNDLOST;
+        return LOST;
     }
     else
     {
-        if(computerChoice == 1) return ROUNDLOST;
-        else if(computerChoice == 2) return ROUNDWON;
+        if(computerChoice == 1) return LOST;
+        else if(computerChoice == 2) return WIN;
         return DRAW;
     }
 }
+
+function game(rounds = 5)
+{
+    let playerWins = 0;
+    let computerWins = 0;
+    let choice;
+    let play;
+    while(true)
+    {
+        if(playerWins + computerWins == rounds)
+        {
+            if(playerWins == computerWins) return DRAW;
+            else if(playerWins > computerWins) return WIN;
+            else return LOST;
+        }
+        else if(playerWins >= rounds/2) return WIN;
+        else if(computerWins >= rounds/2)return LOST;
+
+        choice = prompt("Rock, Paper or Scissors? (0, 1 ,2)");
+
+        play = round(choice, computerChoice(5) % 3);
+        if(play === null)alert("Round: Draw");
+        else if(play)
+        {
+            alert("Round: Won");
+            playerWins++;
+        }
+        else
+        {
+            alert("Round: Lost");
+            computerWins++;
+        }
+    }
+}
+
+let play = game();
+if(play === null)alert("DRAW");
+else if(play)alert("WIN");
+else alert("LOST");
