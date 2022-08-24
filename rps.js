b35b16675efcfb4ad640863890078b99aa261b58
RagnarokMew <temmieisalmightygod@gmail.com>
2022-08-25 02:28:15 +0300

    Make buttons functional

--- rps.js
+++ rps.js
@@ -2,6 +2,14 @@ const WIN = true;
 const LOST = false;
 const DRAW = null;
 
+let rounds;
+if(!rounds)rounds=5;
+let buttons = document.querySelectorAll("button")
+let candles = document.getElementsByClassName("scorebox");
+let status = document.getElementsByClassName("status");
+let playerWins = 0;
+let computerWins = rounds;
+
 function computerChoice(max)
 {
     return Math.floor(Math.random()*max);
@@ -11,23 +19,62 @@ function round(playerChoice, computerChoice)
 {
     if(playerChoice == 1)
     {
-        if(computerChoice == 1) return DRAW;
-        else if(computerChoice == 2) return LOST;
-        return WIN;
+        if(computerChoice == 1) return;
+        else if(computerChoice == 2) computerWins--;
+        else return playerWins++;
     }
     else if(playerChoice == 2)
     {
-        if(computerChoice == 1) return WIN;
-        else if(computerChoice == 2) return DRAW;
-        return LOST;
+        if(computerChoice == 1) playerWins++;
+        else if(computerChoice == 2) return;
+        else computerWins++;
     }
     else
     {
-        if(computerChoice == 1) return LOST;
-        else if(computerChoice == 2) return WIN;
-        return DRAW;
+        if(computerChoice == 1) computerWins++;
+        else if(computerChoice == 2) playerWins++;
+        else return;
     }
 }
+
+buttons[0].addEventListener('click',()=>
+{
+    round(1,computerChoice(5) % 3); 
+    if(playerWins>rounds/2)
+    {
+        alert("Win");
+    }
+    if(computerWins<rounds/2)
+    {
+        alert("Lose");
+    }
+});
+
+buttons[1].addEventListener('click',()=>
+{
+    round(2,computerChoice(5) % 3); 
+    if(playerWins>rounds/2)
+    {
+        alert("Win");
+    }
+    if(computerWins<rounds/2)
+    {
+        alert("Lose");
+    }
+});
+
+buttons[2].addEventListener('click',()=>
+{
+    round(3,computerChoice(5) % 3); 
+    if(playerWins>rounds/2)
+    {
+        alert("Win");
+    }
+    if(computerWins<rounds/2)
+    {
+        alert("Lose");
+    }
+});
 
 function game(rounds = 5)
 {
@@ -62,8 +109,3 @@ function game(rounds = 5)
         }
     }
 }
-
-let play = game();
-if(play === null)alert("DRAW");
-else if(play)alert("WIN");
-else alert("LOST");
