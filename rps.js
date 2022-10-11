62965bedcaae55d08a7242f3024671af340f94f9
RagnarokMew <temmieisalmightygod@gmail.com>
2022-10-11 15:18:45 +0300

    Add comments for code readability

--- rps.js
+++ rps.js
@@ -1,46 +1,66 @@
+
+// Constants for game status
 const WIN = true;
 const LOST = false;
 const DRAW = null;
 
+// Possible Computer names
 const names = ["Sir Knightsalot","Knight-chan","Hotspur","The Blademaster","The Berserk", "The Crafty", "El Cid", "The Lionhearted", "The Black Prince", "The Eagle of Brittany"];
 const nNames=10;
 
+//Possible Text for buttons: What they are & game status
 const choices = ['Rock', 'Paper', 'Scissors'];
 const endingWin  = ['YOU', 'HAVE', 'WON'];
 const endingLoss = ['YOU', 'HAVE', 'LOST'];
 
+//Names for player and computer
 let playerName = document.getElementsByClassName("name")[0];
 let computerName = document.getElementsByClassName("name")[1];
 
+//Game Stats
 let rounds = 5;
 let playerWins = 0;
 let computerWins = rounds - 1;
 
+//Visualisers for score (the candles)
 let candles = document.getElementsByClassName("scorebox");
 candles = Array.from(candles);
 
-let phaseStatus = document.getElementsByClassName("status")[0];
-let currPhase = "Attack Phase";
-let messagePhase = document.getElementById("msg");
+//Round Statuses
+let phaseStatus = document.getElementsByClassName("status")[0]; //Status for round
+let currPhase = "Attack Phase";                                 //Phase status message (no functionality)
+let messagePhase = document.getElementById("msg");              //Phase status DOM
 
+//Counter for score player & computer
 let scores = document.getElementsByClassName("counter");
 
-let buttons = document.getElementsByClassName("btnhdl")[0].children;
+//Buttons
+let buttons = document.getElementsByClassName("btnhdl")[0].children;    //Rock, Paper, Scissors Buttons
 buttons= Array.from(buttons);
-let startbtn = document.getElementById("start");
+
+//Menu Buttons
+let startbtn = document.getElementById("start");        //Main Menu Start Button
+
+//Lose Screen Buttons
 let menubtn1 = document.getElementById("mainmenu1");
-let menubtn2 = document.getElementById("mainmenu2");
 let startbtn1 = document.getElementById("reset1");
+
+//Win Screen Buttons
+let menubtn2 = document.getElementById("mainmenu2");
 let startbtn2 = document.getElementById("reset2");
 
+//Menus
 let startScreen = document.getElementsByClassName("start")[2];
 let defeatScreen = document.getElementsByClassName("start")[0];
 let winScreen = document.getElementsByClassName("start")[1];
 
+//Only Main Menu Screen is displayed on start
 defeatScreen.style.display = "none";
 winScreen.style.display = "none";
 
 animate(startScreen);
+
+//Start Buttons Events
 
 startbtn.addEventListener('click', ()=>
 {
@@ -63,6 +83,8 @@ startbtn2.addEventListener('click', ()=>
     winScreen.style.display = "none";
 });
 
+//To Menu Buttons Events
+
 menubtn1.addEventListener('click', ()=>
 {
     startScreen.style.display = "flex";
@@ -77,7 +99,7 @@ menubtn2.addEventListener('click', ()=>
     winScreen.style.display = "none";
 });
 
-
+//Rock Paper Scissors Buttons Events
 
 buttons[0].addEventListener('click',()=>
 {
@@ -118,6 +140,8 @@ buttons[2].addEventListener('click',()=>
     }
 });
 
+
+//Animates children with fin with fade_in
 function animate(parent)
 {
     let elements = parent.querySelector(".cwrap");
@@ -132,7 +156,8 @@ function animate(parent)
 
 }
 
-function fade_in(target, index, opacity=0)
+//Fade in animation
+function fade_in(target, index=0, opacity=0)
 {
     if(opacity<1)
     {
@@ -143,6 +168,13 @@ function fade_in(target, index, opacity=0)
     }
 }
 
+//Random Number function
+function computerChoice(max)
+{
+    return Math.floor(Math.random()*max);
+}
+
+//Changes phase status
 function phase()
 {
     if(computerChoice(2)== true)
@@ -153,6 +185,7 @@ function phase()
     }
 }
 
+//Locks buttons and displays correct Menu (Win / Defeat)
 function lock(ending)
 {
     buttons.forEach((button,index)=>
@@ -173,11 +206,7 @@ function lock(ending)
     }
 }
 
-function computerChoice(max)
-{
-    return Math.floor(Math.random()*max);
-}
-
+//Adds candle light to candle
 function lightUp(candle)
 {
     const fire = document.createElement('img');
@@ -186,6 +215,7 @@ function lightUp(candle)
     candles[candle].appendChild(fire);
 }
 
+//Removes all candle lights
 function lightDown()
 {
     candles.forEach((candle, index)=>
@@ -196,6 +226,7 @@ function lightDown()
     phaseStatus.textContent ="";
 }
 
+//Handles a round (Each call represents a round)
 function round(playerChoice, computerChoice)
 {
     if(playerChoice == 1)
@@ -272,6 +303,7 @@ function round(playerChoice, computerChoice)
     }
 }
 
+//Resets Game State
 function reset()
 {
     rounds = 5;
